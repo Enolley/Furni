bdc829ebf95ffef95701bd0b1a0df391b9ce5012
enolley <enolleyomukamani@gmail.com>
2023-10-30 17:03:36 +0300

    Furni

---
+++ client/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = "client"
+urlpatterns = [
+    path('', views.index, name="Index"),
+    path('shop', views.shop, name="Shop"),
+    path('about', views.about, name="About"),
+    path('services', views.services, name="Services"),
+    path('blog', views.blog, name="Blog"),
+    path('contact', views.contact, name="Contact"),
+    path('cart', views.cart, name="Cart"),
+    path('checkout', views.checkout, name="Checkout")
+
+]
---
+++ client/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html', {'navbar': 'index'})
+
+
+def shop(request):
+    return render(request, 'shop.html', {'navbar': 'shop'})
+
+
+def about(request):
+    return render(request, 'about.html', {'navbar': 'about'})
+
+
+def services(request):
+    return render(request, 'services.html', {'navbar': 'services'})
+
+
+def blog(request):
+    return render(request, 'blog.html', {'navbar': 'blog'})
+
+
+def contact(request):
+    return render(request, 'contact.html', {'navbar': 'contact'})
+
+def cart(request):
+    return render(request, 'cart.html', {'navbar' : 'cart'})
+
+def checkout(request):
+    return render(request, 'checkout.html', {'navbar' : 'checkout'})
